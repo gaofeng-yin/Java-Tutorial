8aa2a558ed209111997a7410feaef4282aeba30a
gaofeng <gaofengyin@hotmail.com>
2020-10-14 23:28:35 +0100

    variables, for , if & else , while and do while

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,58 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
+		/* Print hello world
 		System.out.println("Hello world!");
-
+		*/
+		
+		/* create a variable and then print it in console
+		int number = 23;
+		
+		String str = "This is string!";
+		
+		char ch = 'c';
+		
+		System.out.println(number);
+		System.out.println(str);
+		System.out.println(ch);
+		*/
+		
+		/* cicle for 
+		for(int i = 0; i <= 10; i++) {
+			System.out.println(i);
+		}
+		*/
+		
+		/* if and else statement
+		int number = 7;
+		
+		if(number < 4) {
+			System.out.println("Number is greater than 4!");
+		}else {
+			System.out.println("Chose other number!");
+		}
+		*/
+		
+		/* while statement
+		int i = 0;
+		
+		while(i < 10) {
+			System.out.println("I'm too small, i need level up!");
+			i++;
+		}
+		
+		System.out.println("Finally i'm level " + i +"!!");
+		*/
+		
+		/* do while statement
+		int d = 0;
+		
+		do {
+			d++;
+		} while(d < 10);
+		
+		System.out.println("Ups, i'm level " + d + "!!");
+		*/
 	}
 
 }
